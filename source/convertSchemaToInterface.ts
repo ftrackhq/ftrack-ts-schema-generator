08db18b8929f195d542124210e6e0268b1d5363c
Lars Johansson <gismya@gmail.com>
2023-11-21 11:21:44 +0100

    Fix: Incorrect schema response file

--- source/convertSchemaToInterface.ts
+++ source/convertSchemaToInterface.ts
@@ -125,9 +125,9 @@ function convertPropertiesToTypes(
       );
     }
     // If neither type or $ref is defined, we can't generate a type. Log an error
-    if (!("type" in value) && !("$ref" in value) && !("alias_for" in value)) {
+    if (!("type" in value) && !("$ref" in value)) {
       errors.push(
-        `No type or $ref defined for property ${key} in schema ${schema.id}`
+        `No type or $ref defined for property ${key} in schema ${schema.id}`,
       );
     }
 
